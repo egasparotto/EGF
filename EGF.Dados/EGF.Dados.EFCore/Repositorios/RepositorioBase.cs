4f56f51ba0acb6ecae487868176b058f71904c3a
Eduardo Gasparotto <gasp.eduardo@gmail.com>
2020-07-11 16:22:54 -0300

    Criado o repositório com Id

--- EGF.Dados/EGF.Dados.EFCore/Repositorios/RepositorioBase.cs
+++ EGF.Dados/EGF.Dados.EFCore/Repositorios/RepositorioBase.cs
@@ -9,32 +9,32 @@ namespace EGF.Dados.EFCore.Repositorios
     public abstract class RepositorioBase<TEntidade> : IRepositorioBase<TEntidade>
         where TEntidade : EntidadeBase
     {
-        public IEnumerable<TEntidade> Buscar()
+        public virtual IEnumerable<TEntidade> Buscar()
         {
             throw new NotImplementedException();
         }
 
-        public IEnumerable<TEntidade> Buscar(Func<TEntidade, bool> pesquisa)
+        public virtual IEnumerable<TEntidade> Buscar(Func<TEntidade, bool> pesquisa)
         {
             throw new NotImplementedException();
         }
 
-        public TEntidade Editar(TEntidade entidade)
+        public virtual TEntidade Editar(TEntidade entidade)
         {
             throw new NotImplementedException();
         }
 
-        public TEntidade Inserir(TEntidade entidade)
+        public virtual TEntidade Inserir(TEntidade entidade)
         {
             throw new NotImplementedException();
         }
 
-        public int NumeroDeRegistros()
+        public virtual int NumeroDeRegistros()
         {
             throw new NotImplementedException();
         }
 
-        public void Remover(TEntidade entidade)
+        public virtual void Remover(TEntidade entidade)
         {
             throw new NotImplementedException();
         }
---
+++ EGF.Dados/EGF.Dados.EFCore/Repositorios/RepositorioComId.cs
@@ -0,0 +1,19 @@
+﻿using EGF.Dominio.Entidades;
+using EGF.Dominio.Repositorios;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EGF.Dados.EFCore.Repositorios
+{
+    public abstract class RepositorioComId<TEntidade> : RepositorioBase<TEntidade>, IRepositorioComId<TEntidade>
+        where TEntidade:EntidadeComId
+    {
+        public virtual TEntidade ObterPorId(int id)
+        {
+            return Buscar(x => x.Id == id).FirstOrDefault();
+        }
+    }
+}
