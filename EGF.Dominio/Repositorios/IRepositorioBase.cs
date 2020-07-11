b81c1879ec7547da31ebc8cb596a2ffab4c45f4e
Eduardo Gasparotto <gasp.eduardo@gmail.com>
2020-07-11 16:07:41 -0300

    Incluído o repositório base

---
+++ EGF.Dominio/Repositorios/IRepositorioBase.cs
@@ -0,0 +1,18 @@
+﻿using EGF.Dominio.Entidades;
+
+using System;
+using System.Collections.Generic;
+
+namespace EGF.Dominio.Repositorios
+{
+    public interface IRepositorioBase<TEntidade>
+        where TEntidade: EntidadeBase
+    {
+        public IEnumerable<TEntidade> Buscar();
+        public IEnumerable<TEntidade> Buscar(Func<TEntidade, bool> pesquisa);
+        public TEntidade Inserir(TEntidade entidade);
+        public TEntidade Editar(TEntidade entidade);
+        public void Remover(TEntidade entidade);
+        public int NumeroDeRegistros();
+    }
+}
