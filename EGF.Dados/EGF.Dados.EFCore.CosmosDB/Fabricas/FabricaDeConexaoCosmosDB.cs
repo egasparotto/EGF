cc3dc6991dba6ef7aa83197a5cdcad67aba22750
Eduardo Gasparotto <gasp.eduardo@gmail.com>
2021-02-22 21:06:04 -0300

    Incluído suporte ao CosmosDB no EGF

---
+++ EGF.Dados/EGF.Dados.EFCore.CosmosDB/Fabricas/FabricaDeConexaoCosmosDB.cs
@@ -0,0 +1,27 @@
+﻿using EGF.Dados.EFCore.Fabricas;
+using EGF.Licenciamento.Core.Licencas.Gerenciadores;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGF.Dados.EFCore.CosmosDB.Fabricas
+{
+    public class FabricaDeConexaoCosmosDB : FabricaDeConexao
+    {
+        public FabricaDeConexaoCosmosDB(IGerenciadorDeLicenca gerenciadorDeLicenca) : base(gerenciadorDeLicenca)
+        {
+        }
+
+        public override DbContextOptions Fabricar()
+        {
+            DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
+            builder.UseCosmos(Licenca.ServidorBanco, Licenca.NomeBanco);
+            return builder.Options;
+        }
+    }
+}
